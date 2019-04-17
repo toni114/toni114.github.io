e9fd466c50eabd63395cbf0209eff68e2e01d187
toni114 <designingning@gmail.com>
2019-04-17 10:09:12 +0800

    Add files via upload

---
+++ js/2333.js
@@ -0,0 +1,10 @@
+
+        function checkpassword(){
+		var abc=(1998+1997)*666-(300*555)+17676460//算你厉害，看到这里惹！;
+        var pwd=String(abc);
+        var inpwd=document.getElementById("password").value;//获取输入的代码
+        if(pwd == inpwd){  //判断一下是否一样
+            window.location.href="arkie.html";
+    }
+}
+
