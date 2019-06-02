bb3ef50888b422328ae2bd00d4d77032519b770e
toni114 <designingning@gmail.com>
2019-06-03 00:40:20 +0800

    Add files via upload
    v8.1

---
+++ js/loader.js
@@ -0,0 +1,74 @@
+
+
+//header
+document.getElementById("tou").innerHTML = `<div class="navbar navbar-default navbar-static-top" role="navigation">
+<div class="container">
+
+     <div class="navbar-header">
+          <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+               <span class="icon icon-bar"></span>
+               <span class="icon icon-bar"></span>
+               <span class="icon icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html"><img src="https://ning114.oss-cn-hongkong.aliyuncs.com/logo.png" height=45px width=45px></a>
+     </div>
+<!-- 导航 -->
+     <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+               <li><a href="work.html">Work/工作场</a></li>
+               <li><a href="lab.html">Lab/实验田 </span></a></li>
+               <li><a href="think.html">Think/自习室</a></li>
+               <li><a href="aboutme.html">About me/关于我</a></li>
+           
+          </ul>
+     </div>
+
+</div>
+</div>`
+
+//footer
+document.getElementById("pigu").innerHTML =`<footer style="padding:70px 0px ">
+<div class="container">
+     <div class="row">
+
+          <div class="col-md-3 col-sm-3">
+          <p>© 2019 Ning | All Rights Reserved. </p> 
+          </div>
+
+          <div class="col-md-4 col-sm-4">
+               
+             <div class="footer-copyright">
+                   
+             </div>  
+          </div>
+
+          <div class="col-md-offset-1 col-md-4 col-sm-offset-1 col-sm-3">
+               <ul class="social-icon">
+                
+                
+                    <li><a href="images/weixin.PNG"  class="fa fa-weixin"> </a> </li>
+                   
+                    <li><a href="https://www.linkedin.com/in/%E5%AE%81-%E5%94%90-b24607117/" target="_blank" class="fa fa-linkedin"></a></li>
+                    <li><a href="https://www.facebook.com/profile.php?id=100010861092152" target="_blank" class="fa fa-facebook"> </a> </li>
+                   
+                    <li><a href="http://ningning2333.lofter.com/" target="_blank" class="fa fa-coffee"> </a></li>
+               </ul>
+           
+              
+          </div>
+
+          <div class="clearfix col-md-12 col-sm-12" style="height:50px">
+               <hr>
+          </div>
+
+          <div class="col-md-6 col-sm-6">
+               
+          </div>
+
+          <div class="col-md-6 col-sm-6">
+              
+          </div>
+          
+     </div>
+</div>
+</footer>`;
